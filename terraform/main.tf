0732f7c1023be41b3647f35303be4bb9030f5395
Andrey Rojas <andrey.rojas@gorillalogic.com>
2020-05-08 20:14:02 -0600

    fix main.tf issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "swarm-infra-be-instances" {
 
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "10.0.1.0/24"
-  vpc_id            = "${vpc_jenkins}"
+  vpc_id            = "${var.vpc_jenkins}"
   availability_zone = "${var.region}a"
 
   tags = {
@@ -43,7 +43,7 @@ resource "aws_subnet" "public-subnet-1" {
 }
 
 resource "aws_internet_gateway" "jenkins-vpc-ig" {
-  vpc_id = "${vpc_jenkins}"
+  vpc_id = "${var.vpc_jenkins}"
 
   tags = {
       Name = "Jenkins-vpc-internet-gateway"
@@ -51,7 +51,7 @@ resource "aws_internet_gateway" "jenkins-vpc-ig" {
 }
 
 resource "aws_route_table" "jenkins-route-table" {
-  vpc_id = "${vpc_jenkins}"
+  vpc_id = "${var.vpc_jenkins}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -71,7 +71,7 @@ resource "aws_route_table_association" "jenkins-subnet-rt-associate" {
 resource "aws_security_group" "sg_frontend" {
   name        = "frontend_rules"
   description = "Allow SSH and Angular inbound traffic"
-  vpc_id      = "${vpc_jenkins}"
+  vpc_id      = "${var.vpc_jenkins}"
 
   ingress {
     from_port   = 22
@@ -98,7 +98,7 @@ resource "aws_security_group" "sg_frontend" {
 resource "aws_security_group" "sg_backend" {
   name        = "backend_rules"
   description = "Allow SSH and Spring Boot inbound traffic"
-  vpc_id      = "${vpc_jenkins}"
+  vpc_id      = "${var.vpc_jenkins}"
 
   ingress {
     from_port   = 22

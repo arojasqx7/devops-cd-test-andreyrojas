ec1354e50ee59827688394f734f363be8bf908b2
Andrey Rojas <andrey.rojas@gorillalogic.com>
2020-05-09 12:37:11 -0600

    adjust jenkinsfile for tf lock aws creds

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,20 +122,6 @@ resource "aws_security_group" "sg_backend" {
   }   
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-infra-gl-test"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-dblocks-infra"
   billing_mode = "PAY_PER_REQUEST"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,7 +17,3 @@ output "infra_public_subnet_name" {
 output "dynamodb_table_name" {
   value = "${aws_dynamodb_table.terraform_locks.name}"
 }
-
-output "s3_bucket_arn" {
-  value       = "${aws_s3_bucket.terraform_state.arn}"
-}

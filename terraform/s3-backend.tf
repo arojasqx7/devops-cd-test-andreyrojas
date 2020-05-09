0e7bb14d2252bdf4ab002da77d3d2cb9054980cd
Andrey Rojas <andrey.rojas@gorillalogic.com>
2020-05-09 11:49:06 -0600

    adjust jenkinsfile for tf lock aws creds

--- terraform/s3-backend.tf
+++ terraform/s3-backend.tf
@@ -1,9 +1,4 @@
 terraform {
     backend "s3" {
-        bucket         = "terraform-up-and-running-state"
-        key            = "workspaces-example/terraform.tfstate"
-        region         = "us-east-2"
-        dynamodb_table = "terraform-up-and-running-locks"
-        encrypt        = true
     }
 }

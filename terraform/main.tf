4854c1bf089b4a125c23b62906a446ecea56f59d
Andrey Rojas <andrey.rojas@gorillalogic.com>
2020-05-09 09:03:40 -0600

    fix BE region name

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,7 +149,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-infra"
     key            = "global/s3/terraform.tfstate"
-    region         = "${var.region}"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-locks-infra"
     encrypt        = true
   }
